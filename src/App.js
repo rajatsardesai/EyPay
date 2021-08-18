ab0e86e146cfa248715330be887019e97b94bf71
Rajat <rajatsardesai@gmail.com>
2021-08-18 17:45:27 +0530

    Added about us section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar";
 import Header from "./Header";
 import HowItWorks from "./HowItWorks";
+import AboutUs from "./AboutUs";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Header />
       <HowItWorks />
+      <AboutUs />
     </>
   );
 }
--- src/HowItWorks.jsx
+++ src/HowItWorks.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react';
+import workapi from './API/workApi';
 
 const HowItWorks = () => {
+
+    const [workData, setWorkData] = useState(workapi);
+
     return (
         <>
             <section>
@@ -9,13 +13,23 @@ const HowItWorks = () => {
                         How does it work
                     </h1>
                     <div className="row">
-                        <div className="col-12 col-lg-4 text-center work-container-subdiv">
-                            <i class="fas fa-download"></i>
-                            <h2 className="sub-heading">Download App</h2>
-                            <p className="main-hero-para w-100">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus voluptate assumenda facere laboriosam corrupti?
-                            </p>
-                        </div>
+                        {
+                            workData.map((currElem) => {
+                                const { id, logo, title, info } = currElem;
+                                return (
+                                    <>
+                                        <div className="col-12 col-lg-4 text-center work-container-subdiv">
+                                            <i class={`fontawesome-style ${logo}`}></i>
+                                            <h2 className="sub-heading">{title}</h2>
+                                            <p className="main-hero-para w-100">
+                                                {info}
+                                            </p>
+                                        </div>
+                                    </>
+                                )
+
+                            })
+                        }
                     </div>
                 </div>
             </section>
